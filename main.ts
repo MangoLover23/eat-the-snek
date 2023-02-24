d78da2e40c49672f4c55e757e16addd17c9e79e4
Mo Rungjang <112864184+MangoLover23@users.noreply.github.com>
2023-02-24 12:33:23 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let delay = 0
+let stel_direction = 0
+let dxOffset = 0
+let snake = 0
+let apple = 0
+let maxLength = 10
+basic.forever(function () {
+    let score = 0
+    if (game.isGameOver()) {
+    	
+    }
+    delay = Math.max(100, 1000 - score * 50)
+    basic.pause(delay)
 })
