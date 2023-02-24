f424d5a57f5b184101ec5e1012518f07e3509f97
Mo Rungjang <112864184+MangoLover23@users.noreply.github.com>
2023-02-24 12:44:22 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -4,9 +4,13 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
 	
 })
+function letComputerPlay () {
+	
+}
 input.onButtonPressed(Button.B, function () {
 	
 })
+let score = 0
 let delay = 0
 let stel_direction = 0
 let dxOffset = 0
@@ -14,10 +18,15 @@ let snake = 0
 let apple = 0
 let maxLength = 10
 basic.forever(function () {
-    let score = 0
     if (game.isGameOver()) {
     	
     }
     delay = Math.max(100, 1000 - score * 50)
     basic.pause(delay)
+    if (snake[0].isTouching(apple)) {
+        if (snake.length < maxLength) {
+            snake.push(snake[snake.length - 1])
+        }
+        score += 1
+    }
 })
